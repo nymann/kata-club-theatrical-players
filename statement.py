8325270cf16fb89b3b1d27ea1a4d4c995d662c28
Kristian Jakobsen <kristian@nymann.dev>
2023-08-20 19:21:19 +0200

    Refactor: Extract function

--- statement.py
+++ statement.py
@@ -11,23 +11,31 @@ def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
         return f"${amount:0,.2f}"
 
     for perf in invoice["performances"]:
+        this_amount = price_for_performance(perf, plays)
         play = plays[perf["playID"]]
-        if play["type"] == "tragedy":
-            this_amount = price_for_tragedy(perf["audience"])
-        elif play["type"] == "comedy":
-            this_amount = price_for_comedy(perf["audience"])
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
+        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
+        total_amount += this_amount
 
         volume_credits += max(perf["audience"] - 30, 0)
         if "comedy" == play["type"]:
             volume_credits += math.floor(perf["audience"] / 5)
-        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
-        total_amount += this_amount
 
     result += f"Amount owed is {format_as_dollars(total_amount/100)}\n"
     result += f"You earned {volume_credits} credits\n"
     return result
+
+
+def price_for_performance(
+    perf: dict[str, Any],
+    plays: dict[str, Any],
+) -> float:
+    play = plays[perf["playID"]]
+
+    if play["type"] == "tragedy":
+        return price_for_tragedy(perf["audience"])
+    if play["type"] == "comedy":
+        return price_for_comedy(perf["audience"])
+    raise ValueError(f'unknown type: {play["type"]}')
 
 
 def price_for_tragedy(audience: int) -> float:
